5e3dfd43c02b4f1882ed5e6ddebd3852291144de
LinusMatysiak <Matysiaklinus@gmail.com>
2022-03-23 09:59:22 +0100

    zadanie na lekcji NWW oraz NWD

---
+++ NWD.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a;
+	cin >> b;
+	while (a != b) {
+
+		if (a < b)
+		{
+			b = b - a;
+		}
+		else
+		{
+			a = a - b;
+		}
+	}
+	cout << a;
+}
---
+++ NWW.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, nww;
+	cin >> a;
+	cin >> b;
+	c = a * b;
+	while (a != b) {
+
+		if (a < b)
+		{
+			b = b - a;
+		}
+		else
+		{
+			a = a - b;
+		}
+	}
+	nww = c / a;
+	cout << nww;
+}
