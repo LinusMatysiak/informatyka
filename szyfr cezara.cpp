7d013a768c10d7fcf500b4a847231782c04106ff
LinusMatysiak <Matysiaklinus@gmail.com>
2022-04-06 09:56:57 +0200

    szyfr cezara

---
+++ szyfr cezara.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+void szyfruj(int klucz, char* tekst);
+int main()
+{
+    char tekst[200];
+    int klucz;
+
+    cout << "podaj tekst \n";
+    cin >> tekst;
+    cout << "podaj klucz \n";
+    cin >> klucz;
+    szyfruj(klucz, tekst);
+}
+void szyfruj(int klucz, char* tekst) 
+{
+    int dl = strlen(tekst);
+    if (!(klucz >= -26 && klucz <=26)) 
+    {
+        return;
+    }
+
+    if (klucz >= 0)
+    {
+        for (int i = 0; i < dl;i++)
+        {
+            if (tekst[i] + klucz <= 'z')
+                tekst[i] += klucz;
+            else
+            {
+                tekst[i] = tekst[i] + klucz - 26;
+            }
+        }
+    }
+    else 
+    {
+        for (int i =0 ;i < dl; i++)
+        {
+            if (tekst[i] + klucz >= 'a')
+                tekst[i] += klucz;
+            else
+            {
+                tekst[i] = tekst[i] + klucz + 26;
+            }
+        }
+    }
+    cout << tekst;
+}
