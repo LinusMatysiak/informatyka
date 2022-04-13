cb0ac7f30262a3030ec728140db9d4bd919fa168
LinusMatysiak <Matysiaklinus@gmail.com>
2022-04-13 09:51:37 +0200

    Ciąg Fibonacciego

---
+++ Ciąg Fibonacciego.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int F0;
+    int F1;
+    int F;
+    cout << "ile liczb chcesz wypisać ";
+    cin >> n;
+    F0 = 0;
+    F1 = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 1)
+        {
+            F = F0 + F1;
+            F0 = F1;
+            F1 = F;
+        }
+        else
+        {
+            F = i;
+        }
+        cout << F << " ";
+    }
+}
