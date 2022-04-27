9ae7fb1c0c12ca2cadab7bb94a90328939ae28a9
LinusMatysiak <Matysiaklinus@gmail.com>
2022-04-27 09:48:13 +0200

    wyszukiwanie binarne

---
+++ wyszukiwanie binarne.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int funkcja(int szukana, int tab[]);
+int main()
+{
+	int szukana;
+	int tab[15] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };
+	cout << "wpisz liczbe szukaną: ";
+	cin >> szukana;
+	cout << "\n";
+	funkcja(szukana, tab);
+	if (funkcja(szukana, tab) == -1)
+	{
+		cout << "nie znaleziono szukanej";
+	}
+	else
+	{
+		cout << "szukana na pozycji: " << funkcja(szukana, tab) +1;
+	}
+}
+int funkcja(int szukana, int tab[]) 
+{
+	int l = 0;
+	int p = 15;
+	int sr = (l + p) / 2;
+	while (l <= p)
+	{
+		if (tab[sr] == szukana)
+		{
+			return sr;
+		}
+		if (tab[sr] > szukana)
+		{
+			p = sr - 1;
+		}
+		else
+		{
+			l = sr + 1;
+		}
+		sr = (l + p) / 2;
+	}
+	return -1;
+}
